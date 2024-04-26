985256655ee6e79b739a6462357be944556061bc
flavia1504 <milhouse.mb@gmail.com>
2024-04-25 23:16:49 -0300

    hooks

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import SideBar from './components/SideBar.jsx';
+import './assets/css/app.css'
+
+function App() {
+  return (
+    <>
+      <div id="wrapper">
+          <SideBar />
+        </div>
+    </>
+  );
+}
+
+export default App;
+
+
---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import SideBar from './SideBar.jsx';
+function App() {
+  return (
+    <>
+      <div id="wrapper">
+          <SideBar />
+        </div>
+    </>
+  );
+}
+
+export default App;
